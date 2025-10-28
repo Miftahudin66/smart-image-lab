f15e20ca3f248befa39fb9f3bb5743d1c3e3a3cb
Miftahudin66 <146646295+Miftahudin66@users.noreply.github.com>
2025-10-28 20:25:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image, ImageEnhance
+import io
 
 # Konfigurasi halaman Streamlit
 st.set_page_config(page_title="Smart Image Lab", page_icon="🧠", layout="wide")
@@ -134,4 +139,3 @@ if uploaded_file is not None:
 
 else:
     st.info("Silakan upload gambar terlebih dahulu untuk memulai pengolahan citra.")
-
